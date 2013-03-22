dede73e644b7d410e11d06b3f4c767c55e195433
abhimanyusaxena <abhimanyusaxena@gmail.com>
2013-03-23 00:58:05 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GameAcademy::Application.config.secret_token = '3a444e2647eaa7cd55601aa9146f07c871885ec77ca417fde9792afc69b4741f68811dcdfd92d288888f65b15a718397e9d528459c4c4ed48d8c06887e27b3ff'
